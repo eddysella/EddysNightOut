de35878f5595a47bae68dd682a846da82d72a754
eddy <es463@sussex.ac.uk>
2019-11-10 01:19:13 +0000

    Stable console version :D

--- src/server/egame.py
+++ src/server/egame.py
@@ -1,14 +1,62 @@
-#! /usr/bin/python3
-
-from datetime import datetime
-from twilio.rest import Client
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
 import time
-
 import threading
 
+# twilio phone-numbers:update "+15017122661" --sms-url="http://localhost:5000/sms"
+# use code above to reset tunnel
+num_reply = dict()
+
+app = Flask(__name__)
+
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_ahoy_reply():
+    resp = MessagingResponse()
+    num_reply[request.form['From']] = request.form['Body']
+    resp.message("Response Received")
+    # print(num_reply)
+    return str(resp)
+
+
+def run_vote(objnode):
+    print(objnode.text, end="\n")
+
+    time.sleep(20)
+
+    a, b, c = 0
+
+    for record in num_reply:
+        if num_reply[record] == '1':
+            a += 1
+        elif num_reply[record] == '2':
+            b += 1
+        elif num_reply[record] == '3':
+            c += 1
+
+    num_reply.clear()
+    total = a + b + c
+
+    if total == 0:
+        print("Nobody voted.\n")
+        return objnode
+    else:
+        print(total, " people voted.\n")
+        print("%0.2f" % (a / total * 100), "% voted OPTION 1\n")
+        print("%0.2f" % (b / total * 100), "% voted OPTION 2\n")
+        print("%0.2f" % (c / total * 100), "% voted OPTION 3\n")
+
+    if a > b and a > c:
+        return objnode.a
+    elif b > a and b > c:
+        return objnode.b
+    else:
+        return objnode.c
+
+
 class Game(threading.Thread):
-    def __init__(self, *args, **kwargs):
-        super(Game, self).__init__(*args, **kwargs)
+    def __init__(self):
+        threading.Thread.__init__(self)
 
     class Node:
         a = None
@@ -19,68 +67,39 @@ class Game(threading.Thread):
         def __init__(self, text):
             self.text = text
 
-        def setNodeA(self, node):
+        def set_node_a(self, node):
             self.a = node
 
-        def setNodeB(self, node):
+        def set_node_b(self, node):
             self.b = node
 
-        def setNodeC(self, node):
+        def set_node_c(self, node):
             self.c = node
 
-    def runVote(objnode):
+    def run(self):
+        node = self.Node("I")
+        node.set_node_a(self.Node("am"))
+        node.a.set_node_a(self.Node("dead"))
+        node.a.set_node_b(self.Node("alive"))
+        node.a.set_node_c(self.Node("drunk"))
 
-        begin_es = datetime.now()
+        node.set_node_b(self.Node("have"))
+        node.b.set_node_a(self.Node("herpes"))
+        node.b.set_node_b(self.Node("nothing"))
+        node.b.set_node_c(self.Node("everything"))
 
-        print(objnode.text, end="\n")
-
-        time.sleep(10)
-
-        a = 0
-        b = 0
-        c = 0
-
-        for record in num_reply:
-            if (num_reply[record] == '1'):
-                a += 1
-            elif (num_reply[record] == '2'):
-                b += 1
-            elif (num_reply[record] == '3'):
-                c += 1
-
-        sum = a + b + c
-
-        print(sum, " people voted.\n");
-        print("%0.2f" % (a / sum * 100), "% voted OPTION 1\n")
-        print("%0.2f" % (b / sum * 100), "% voted OPTION 1\n")
-        print("%0.2f" % (c / sum * 100), "% voted OPTION 1\n")
-
-        if (a > b and a > c):
-            return objnode.a
-        elif (b > a and b > c):
-            return objnode.b
-        else:
-            return objnode.c
-
-    def run():
-
-        node = Node("I")
-        node.setNodeA(Node("am"))
-        node.a.setNodeA(Node("dead"))
-        node.a.setNodeB(Node("alive"))
-        node.a.setNodeC(Node("drunk"))
-
-        node.setNodeB(Node("have"))
-        node.b.setNodeA(Node("herpes"))
-        node.b.setNodeB(Node("nothing"))
-        node.b.setNodeC(Node("everything"))
-
-        node.setNodeC(Node("can"))
-        node.c.setNodeA(Node("fly"))
-        node.c.setNodeA(Node("poop"))
-        node.c.setNodeA(Node("giggle"))
+        node.set_node_c(self.Node("can"))
+        node.c.set_node_a(self.Node("fly"))
+        node.c.set_node_a(self.Node("poop"))
+        node.c.set_node_a(self.Node("giggle"))
 
         curr = node
 
-        while (curr is not None):
-            curr = runVote(curr)
+        while curr is not None:
+            curr = run_vote(curr)
+
+
+if __name__ == "__main__":
+    g = Game()
+    g.start()
+    app.run(debug=True)
